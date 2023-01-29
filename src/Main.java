1de82078dc2a554af74bdb7c7dba73d83e75165c
jalig <117361652+jalig@users.noreply.github.com>
2023-01-29 15:18:19 +0400

    Все сделал

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,29 @@ import tasks.api.Type;
 import tasks.task.WeeklyTask;
 import tasks.task.YearlyTask;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
         DailyTask dailyTask = new DailyTask(
                 "Выбросить мусор ",
                 Type.PERSONAL,
-                LocalDate.of(2023, 1, 3).atStartOfDay(),
+                LocalDateTime.of(2024, 3, 1, 14,20),
                 "Отсутсвует ");
         MonthlyTask monthlyTask = new MonthlyTask(
                 "Выучить стримы ",
                 Type.PERSONAL,
-                LocalDate.of(2023, 2, 3).atStartOfDay(),
+                LocalDateTime.of(2024, 3, 1, 14,20),
                 "Учить стримы ");
         WeeklyTask weeklyTask = new WeeklyTask(
-                "Прочитать книгу ",
-                Type.PERSONAL,
-                LocalDate.of(2023, 1, 5).atStartOfDay(),
-                "Да прочти ты уже");
+                "Сделать отчет  ",
+                Type.WORK,
+                LocalDateTime.of(2024, 3, 1, 14,20),
+                "Отчет нужно предоставить на почту руководителю");
         YearlyTask yearlyTask = new YearlyTask(
                 "Прочитать книгу ",
                 Type.PERSONAL,
-                LocalDate.of(2024, 3, 1).atStartOfDay(),
+                LocalDateTime.of(2024, 3, 1, 14,20),
                 "Да прочти ты уже");
 
         TaskService taskService = new TaskService();
